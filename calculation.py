2d250220feb366012f9ea2a580bfd0ab99fed062
asmitap <asmitapokharel525@gmail.com>
2023-04-06 17:18:49 +0545

    Python Programming language Fundamentals

---
+++ calculation.py
@@ -0,0 +1,17 @@
+def addition(num1, num2):
+	sums = num1+num2
+	print(sums)
+
+def subtraction(num1, num2):
+	sub = num1-num2
+	print(sub)
+	
+def multiplication(num1, num2):
+	mul = num1*num2
+	print(mul)
+	
+def divide(num1, num2):
+	div = num1/num2
+	print(div)
+
+
